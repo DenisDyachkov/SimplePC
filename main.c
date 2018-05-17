d8789d57fcfef9dd11d77526badebec083506148
Denis Dyachkov <denisrafydachkov@yandex.ru>
2018-05-18 00:09:02 +0700

    Support arrows and fix interface refresh

--- main.c
+++ main.c
@@ -7,14 +7,64 @@
 #include <signal.h>
 #include <sys/time.h>
 
+void create_timer(double interval);
+void signalhangle(int signal);
+
+int main() {
+    eKeys key_down;
+    int flag;
+    signal(SIGUSR1, signalhangle);
+    signal(SIGALRM, signalhangle);
+    sc_memoryInit();
+    sc_regInit();
+    interface_load(WHITE, PURPLE, CYAN);
+    sc_regSet(FLAG_IGNORE_CLOCK, 1);
+
+    sc_memorySet(36, 0x7FFF);
+
+    while (1) {
+      //pause();
+        interface_print();
+        rk_readkey(&key_down);
+        if (key_down == VK_RUN) {
+            create_timer(1);
+        } else if (key_down == VK_RESET) {
+            create_timer(0);
+            raise(SIGUSR1);
+        } else if (key_down == VK_QUIT) {
+            break;
+        } else if (!sc_regGet(FLAG_IGNORE_CLOCK, &flag) && flag) {
+            switch (key_down) {
+                case VK_DARROW:
+                    if (select_cell < 90)
+                        select_cell += 10;
+                    break;
+                case VK_UARROW:
+                    if (select_cell > 9)
+                        select_cell -= 10;
+                    break;
+                case VK_LARROW:
+                    if (select_cell > 0)
+                        --select_cell;
+                    break;
+                case VK_RARROW:
+                    if (select_cell < 99)
+                        ++select_cell;
+                    break;
+            }
+        }
+    }
+    mt_reset();
+    return 0;
+}
+
 void signalhangle(int signal) {
     switch (signal) {
         case SIGALRM: {
             int val;
             if (!sc_regGet(FLAG_IGNORE_CLOCK, &val) && !val) {
                 ++instructionCounter;
-                printCounter();
-                printf("\n");//fix refresh
+                interface_print();
             }
         }
             break;
@@ -32,34 +82,4 @@ void create_timer(double interval) {
     nval.it_value.tv_sec = nval.it_interval.tv_sec = (long)interval;
     nval.it_value.tv_usec = nval.it_interval.tv_usec = (long)((interval - (long)interval) * 1000000);
     setitimer (ITIMER_REAL, &nval, NULL);
-}
-
-int main() {
-    eKeys key_down;
-    int flag;
-    sc_memoryInit();
-    sc_regInit();
-
-    sc_memorySet(36, 0x7FFF);
-
-    interface_load(WHITE, PURPLE);
-
-    signal(SIGUSR1, signalhangle);
-    signal(SIGALRM, signalhangle);
-
-    while (1) {
-      //pause();
-        rk_readkey(&key_down);
-        if (key_down == VK_RUN) {
-            create_timer(1);
-        } else if (key_down == VK_RESET) {
-            create_timer(0);
-            raise(SIGUSR1);
-        } else if (key_down == VK_QUIT) {
-            break;
-        } else if (!sc_regGet(FLAG_IGNORE_CLOCK, &flag) && flag) {
-            //TODO:
-        }
-    }
-    return 0;
 }
